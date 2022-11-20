cad278e7bd9bae47132a0f8ad18ddae73a75a392
Stephen <stephenswanton@gmail.com>
2022-11-20 10:45:52 +0000

    implementing get current location and add image on create marker

--- app/src/main/java/xyz/stephenswanton/trailapp2/ui/createmarker/CreateMarkerFragment.kt
+++ app/src/main/java/xyz/stephenswanton/trailapp2/ui/createmarker/CreateMarkerFragment.kt
@@ -1,20 +1,29 @@
 package xyz.stephenswanton.trailapp2.ui.createmarker
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+import timber.log.Timber
 import xyz.stephenswanton.trailapp2.R
 import xyz.stephenswanton.trailapp2.databinding.FragmentCreateMarkerBinding
+import xyz.stephenswanton.trailapp2.helpers.showImagePicker
 import xyz.stephenswanton.trailapp2.main.MainApp
+import xyz.stephenswanton.trailapp2.models.Trail
 import xyz.stephenswanton.trailapp2.models.TrailMarker
 import xyz.stephenswanton.trailapp2.models.User
 import xyz.stephenswanton.trailapp2.models.generateRandomId
@@ -55,6 +64,21 @@ class CreateMarkerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (arguments?.getParcelable<TrailMarker>("marker") != null) {
+            edit = true
+            marker = arguments?.getParcelable<TrailMarker>("marker")!!
+            _fragBinding!!.etLatitude.setText(marker.latitude)
+            _fragBinding!!.etLongitude.setText(marker.longitude)
+            _fragBinding!!.etNotes.setText(marker.notes)
+            _fragBinding!!.btnSaveMarker.setText(R.string.save_marker)
+            Picasso.get()
+                .load(marker.image)
+                .into(_fragBinding!!.ivMarkerImage)
+        }
+
+
+
         _fragBinding!!.btnSaveMarker
             .setOnClickListener{
                 marker.latitude = _fragBinding!!.etLatitude.text.toString()
@@ -93,13 +117,72 @@ class CreateMarkerFragment : Fragment() {
                 }
             }
 
-    }
+        _fragBinding!!.btnUseLocation
+            .setOnClickListener{
 
+                locationPermissionRequest.launch(arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION))
+
+                if (ActivityCompat.checkSelfPermission(
+                        requireActivity().applicationContext,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireActivity().applicationContext,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location: Location? ->
+                            _fragBinding!!.etLatitude.setText(location?.latitude.toString())
+                            _fragBinding!!.etLongitude.setText(location?.longitude.toString())
+
+                        }
+                }
+            }
+
+        _fragBinding!!.btnAddImage.setOnClickListener {
+            showImagePicker(imageIntentLauncher)
+        }
+        registerImagePickerCallback()
+    }
+    private fun registerImagePickerCallback() {
+        imageIntentLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result ->
+                when(result.resultCode){
+                    AppCompatActivity.RESULT_OK -> {
+                        if (result.data != null) {
+                            Timber.i("Got Result ${result.data!!.data}")
+                            marker.image = result.data!!.data!!
+                            Picasso.get()
+                                .load(marker.image)
+                                .into(_fragBinding!!.ivMarkerImage)
+                        }
+                    }
+                    AppCompatActivity.RESULT_CANCELED -> { } else -> { }
+                }
+            }
+    }
 
     private fun onSubmitForm(user: User, view: View) {
 
     }
-
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+            } else -> {
+            // No location access granted.
+        }
+        }
+    }
 
 }
 
